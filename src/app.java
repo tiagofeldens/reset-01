47d6a0044e5dba88a9a33b0a1d26534c79a2c4c9
Tiago Feldens <tiagofeldens.tf@gmail.com>
2020-03-07 12:14:01 -0300

    testes

--- src/app.java
+++ src/app.java
@@ -2,6 +2,9 @@ public class app {
     public static void main(String[] args) {
         System.out.println("Hello World");
             System.out.println("Outra Coisa");
+        System.out.println("teste 1");
+        System.out.println("teste 2");
+        System.out.println("teste 3");
 
 
 
